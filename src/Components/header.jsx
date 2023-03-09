665ef892137c604a531420b55eb9ed32af4e8f0a
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-03-09 15:41:42 +0100

    HEADER EXPORT

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,3 +1,17 @@
 import React from "react";
 import Img from "../assets/heaaderlogo.png";
 import { Link } from "react-router-dom";
+
+export default function Header() {
+   return (
+      <div className="header">
+         <nav>
+            <img className="headerLogo" src={Img} alt="Kasa brand logo flagship"></img>
+            <ul>
+               <li><Link to="/" className="items">Accueil</Link></li>
+               <li><Link to="/about" className="items">A Propos</Link></li>
+            </ul>
+         </nav>
+      </div>
+   );
+}
