20dda8e87a72e6dd04303c58b59174e3c28c0a30
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-03-09 15:38:57 +0100

    FOOTER EXPORT

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,2 +1,13 @@
 import React from "react";
 import Img from "../assets/logo.png";
+
+export default function Footer() {
+   return (
+      <div className="footer">
+         <div className="footerLogo">
+            <img src={Img} alt="Kasa brand logo">
+         </div>
+         <p className="footerCopyrights">© 2020 Kasa. All rights reserved</p>
+      </div>
+   );
+}
