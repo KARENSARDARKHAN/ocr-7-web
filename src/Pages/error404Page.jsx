aa94184e7924a36e83e960c8a133d4f1d23cd53a
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-03-10 00:14:42 +0100

    error404Page export

--- src/Pages/error404Page.jsx
+++ src/Pages/error404Page.jsx
@@ -3,3 +3,20 @@ import "../styles/stylesheet.css";
 import Error404 from "../assets/404.png";
 import { Link } from "react-router-dom";
 import Meta from "../components/_ErrorSet";
+
+export default function Error() {
+   return (
+      <div className="error">
+         <Meta title={null} description={null} />
+         <div>
+            <img src={Error404} alt="404 error" />
+         </div>
+         <p className="errorMessage">
+            Oups! La page que vous demandez n'existe pas.
+         </p>
+         <Link to={`/`} className="homeLink">
+            Retour sur la page d'acceuil
+         </Link>
+      </div>
+   );
+}
