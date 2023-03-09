b7c203acce4a7e3df1d82c2beb45eac0cd494200
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-03-10 00:13:52 +0100

    error404Page Imports

---
+++ src/Pages/error404Page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "../styles/stylesheet.css";
+import Error404 from "../assets/404.png";
+import { Link } from "react-router-dom";
+import Meta from "../components/_ErrorSet";
