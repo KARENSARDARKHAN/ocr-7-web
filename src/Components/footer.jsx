586215dfaa9ef3e0328fc1567b5d6b190eb6c3ed
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-03-09 15:39:51 +0100

    FOOTER IMG TAG CORRECTION

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
    return (
       <div className="footer">
          <div className="footerLogo">
-            <img src={Img} alt="Kasa brand logo">
+            <img src={Img} alt="Kasa brand logo"></img>
          </div>
          <p className="footerCopyrights">© 2020 Kasa. All rights reserved</p>
       </div>
