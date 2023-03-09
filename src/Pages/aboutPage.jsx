0a961423958270ade545d8cc0783171db5dd20d5
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-03-10 00:08:35 +0100

    AboutPage Imports

---
+++ src/Pages/aboutPage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import AboutPanel from "../assets/About.png";
+import AboutPanelMobile from "../assets/AboutPanelMobile.png";
+import CollapseTool from "../components/collapse";
+import _errorSet from "../components/_ErrorSet";
+import "../styles/stylesheet.css";
