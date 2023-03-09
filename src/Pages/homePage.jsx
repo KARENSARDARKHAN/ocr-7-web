2301cea87ab9a24ca093a42cad3217fcc8361a5a
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-03-10 00:16:41 +0100

    homePage export

--- src/Pages/homePage.jsx
+++ src/Pages/homePage.jsx
@@ -4,3 +4,47 @@ import Products from "../api/list.json";
 import _errorSet from "../components/_ErrorSet";
 import HomeImg from "../assets/Home.png";
 import "../styles/stylesheet.css";
+
+export default function WelcomePageLayout() {
+   const [currImg] = useState(0);
+   const TitleMeta =
+     "Kasa : location d'appartements entre particuliers";
+   const DescriptionMeta =
+     "Kasa est la première agence en ligne de location d'appartements entre particuliers en France";
+
+   return (
+      <div className="home">
+         <_errorSet title={TitleMeta} description={DescriptionMeta} />
+
+         <section className="WelcomePanel">
+            <p>Chez vous, partout et ailleurs</p>
+            <div className="WelcomePanelPicture">
+               <img src={HomeImg} alt="Slogan panel"></img>
+            </div>
+         </section>
+
+         <div className="TilesList">
+            {Products &&
+               Products.map((product) => (
+                  <Link
+                     to={`/AccomodationInfo/${product.id}`}
+                     className="ItemInfo"
+                     key={product.id}>
+                     
+                     <img
+                        src={product.pictures[currImg]}
+                        alt="Accomodation"
+                        className="ItemPicture"/>
+                     
+                     <div className="overlay"/>
+
+                     <div className="ItemTitle">
+                        <span>{product.title}</span>
+                     </div>
+                     
+                  </Link>
+               ))}
+         </div>
+      </div>
+   );
+}
