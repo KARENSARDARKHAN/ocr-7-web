7cbc106b5e2b739645796b2c1638561e07ce6cfb
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-03-09 15:27:04 +0100

    _ ERROESET EXPORT

---
+++ src/Components/_ErrorSet.jsx
@@ -0,0 +1,7 @@
+export default function _errorSet({ title, description }) {
+   const el = document.querySelector("title");
+   if (title) {
+      el.innerText = `${title}`;
+   } else {
+      el.innerText = Page d'erreur;
+   }
