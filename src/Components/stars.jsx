6f1b7659a307c8e8216c5cf32958b35c243bc5c9
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-03-09 15:50:31 +0100

    STARS EXPORT

--- src/Components/stars.jsx
+++ src/Components/stars.jsx
@@ -1,3 +1,19 @@
 import React from "react";
 import Star from "../assets/Star.png";
 import EmptyStar from "../assets/EmptyStar.png";
+
+export default function Evaluation (rate) {
+   const range = [1, 2, 3, 4, 5];
+
+   return range.map((rangeElem, key) =>
+      rangeElem <= rate.rate ? (
+         <img src={Star} alt="Full star" key={key} />
+      ) : (
+         <img
+            src={EmptyStar}
+            alt="Empty star"
+            key={key}
+            className="emptyStar"/>
+      )
+   );
+}
