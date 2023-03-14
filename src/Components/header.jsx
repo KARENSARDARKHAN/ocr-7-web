569d131bb708542de07c796077a2c2129956f46b
KARENSARDARKHAN <108447157+KARENSARDARKHAN@users.noreply.github.com>
2023-03-14 01:03:47 +0100

    NavLink import on header.jsx and Accueil A Propos links stay underlined when page is active

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import Img from "../assets/heaaderlogo.png";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
    return (
       <div className="header">
          <nav>
-            <img className="headerLogo" src={Img} alt="Kasa brand logo flagship"></img>
+            <img
+               className="headerLogo"
+               src={Img}
+               alt="Kasa brand logo flagship"
+            ></img>
             <ul>
-               <li><Link to="/" className="items">Accueil</Link></li>
-               <li><Link to="/about" className="items">A Propos</Link></li>
+               <li>
+                  <NavLink
+                     to="/"
+                     style={({ isActive }) => ({
+                        textDecoration: isActive ? "underline" : "none",
+                     })}
+                     className="items">
+                     Accueil
+                  </NavLink>
+               </li>
+               <li>
+                  <NavLink
+                     to="/about"
+                     style={({ isActive }) => ({
+                        textDecoration: isActive ? "underline" : "none",
+                     })}
+                     className="items">
+                     A Propos
+                  </NavLink>
+               </li>
             </ul>
          </nav>
       </div>
    );
 }
+
--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { NavLink } from "react-router-dom";
 import WelcomePageLayout from "./Pages/homePage";
 import About from "./Pages/aboutPage";
 import Error from "./Pages/error404Page";
@@ -9,34 +8,10 @@ import Header from "./Components/header";
 import Footer from "./Components/footer";
 
 
-const Users = () => <div>Users</div>;
-const Posts = () => <div>Posts</div>;
-
 function Routing() {
    return (
       <div className="App">
          <Header />
-         <div className="nav">
-            <NavLink
-               to="/"
-               style={({ isActive }) => ({
-                  color: isActive ? "#fff" : "#545e6f",
-                  background: isActive ? "#7600dc" : "#f0f0f0",
-               })}
-            >
-               Acceuil
-            </NavLink>
-            <NavLink
-               to="/about"
-               style={({ isActive }) => ({
-                  color: isActive ? "#fff" : "#545e6f",
-                  background: isActive ? "#7600dc" : "#f0f0f0",
-               })}
-            >
-               A Propos
-            </NavLink>
-         </div>
-
          <Routes>
             <Route path="/" element={<WelcomePageLayout />} />
             <Route
